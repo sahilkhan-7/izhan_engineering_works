93a27a5f942f7fc5dd0f3c499edad3c72b84a6e8
Sahil Khan <sahilkhan782466@gmail.com>
2026-06-21 18:00:37 +0530

    chore: remove sensitive files and local db from tracking

--- check.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function check() {
-  const products = await prisma.product.findMany({ select: { name: true, youtubeUrl: true } });
-  console.log(products);
-}
-
-check().finally(() => prisma.$disconnect());
